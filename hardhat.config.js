e84a0c003b241941c644563b8276bc85ebf6b146
Daniele <danielemeno96@gmail.com>
2023-01-24 10:58:48 +0100

    getWeth script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,45 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.INFURA_MAINNET_RPC_URL || ""
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+
 module.exports = {
-  solidity: "0.7.3",
-};
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8"
+            },
+            {
+                version: "0.6.12"
+            },
+            {
+                version: "0.4.19"
+            }
+        ]
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL
+            }
+        },
+        localhost: {
+            chainId: 31337
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6
+        }
+    }
+}
